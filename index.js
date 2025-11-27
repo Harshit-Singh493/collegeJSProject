79fc188799efc2e3bc0f3de08bc49c6e789ad3d0
Harshit Singh <harshitsingh1706@gmail.com>
2025-11-27 12:01:08 +0530

    Fixing questions Order

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// debugger;
 const log = console.log
-const subBtn = document.getElementById("submit");
-
-
+const subBtn = document.getElementById("submit")
+const backBtn = document.getElementById('goBack')
+const ans = []
+const opt = document.querySelector('input[name="ans"]:checked')
+/*
+Objective of fetchQues() function is to fetch questions from question.json and store 15 random question without repeatation out of it and push it in arr[] and returning
+*/
 async function fetchQues() {
   const res = await fetch("./src/questions.json")
   const data = await res.json()
@@ -9,7 +14,7 @@ async function fetchQues() {
   const arr = []
 
   while (unique.size < 15) {
-    const random = Math.floor(Math.random() * 50) //12 , 41
+    const random = Math.floor(Math.random() * 50) 
     if (!unique.has(random)) {
       unique.add(random)
       arr.push(data[random])
@@ -18,18 +23,20 @@ async function fetchQues() {
   return arr
 }
 
-function loadQues(q, i) {
+
+/*
+Objective of loadQues() function is to display the returned array by fetchQues() questions via DOM
+*/
+function loadQues(q, i, pre) {
   document.getElementById('quesNum').innerHTML = `<h1>Question ${i + 1} : </h1>`
   document.getElementById('mainQuestion').innerHTML =`Q: ${q[i].question}`
+
   document.getElementById('ans1').innerHTML = q[i].options[0]
   document.getElementById('ans2').innerHTML = q[i].options[1]
   document.getElementById('ans3').innerHTML = q[i].options[2]
   document.getElementById('ans4').innerHTML = q[i].options[3]
-  const backBtn = document.getElementById('goBack')
-  document.querySelectorAll('input[name="ans"]').forEach(radio => {
-    radio.checked = false;
-  })
-
+  
+  
   if (q[i].tags == 'HTML') {
 
     document.getElementById('langBadge').innerHTML = `<span class="inline-flex items-center rounded-md bg-red-400/10 px-2 py-1 text-xs font-medium text-red-400 inset-ring inset-ring-red-400/20">${q[i].tags}</span>`
@@ -49,49 +56,62 @@ function loadQues(q, i) {
   } else if (q[i].tags == 'React') {
 
     document.getElementById('langBadge').innerHTML = `<span class="inline-flex items-center rounded-md bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30">${q[i].tags}</span>`
-
-  }
-  if (i == 15) {
-    document.getElementById('submit').innerText = 'Submit'
   }
 
-  backBtn.addEventListener('click', () => {
-    if (i <= 0) {
-      i = 1
-    }
+ 
+  document.querySelectorAll('input[name="ans"]').forEach(radio => radio.checked = false)
 
-    loadQues(q, i - 1)
-  })
+  if (ans[i]) {
+    document.getElementById(ans[i]).checked = true
+  }
 }
 
 async function initQuiz() {
   const questions = await fetchQues()
   let index = 0
   let score = 0
+
   loadQues(questions, index)
 
   subBtn.addEventListener('click', () => {
-
     const opt = document.querySelector('input[name="ans"]:checked')
+    
     if (!opt) {
       alert("Please Select an option")
       return;
-    } else if (opt.id == questions[index].answer) {
-      score++
     }
 
-    
+    ans[index] = opt.id
 
-    index++
+    if (opt.id == questions[index].answer) {
+      score++
+      ans.push = opt.id
+    }
+
+    ans[index] = opt.id
+  
+    index++   
 
     if (index == 15) {
       alert("Quiz Completed")
       localStorage.setItem("quizScore", score)
       window.location.href = `./result.html`
     }
+    
     loadQues(questions, index)
   })
 
+  backBtn.addEventListener('click', () => {
+    log(ans)
+    
+    if (index > 0) {
+      index--
+      loadQues(questions, index)
+    }
+    
+  })
+
+  
 }
 
 initQuiz()
@@ -115,3 +135,5 @@ userTheme.addEventListener('change', () => {
     document.getElementById('theme').style.color = 'white'
   }
 })
+
+
